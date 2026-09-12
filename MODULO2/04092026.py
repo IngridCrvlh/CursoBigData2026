1f1710f6a2cb00562cd08c31e65788918bd1e9d5
INGRID CARVALHO REBELO SILVA <ingrid.rebelo@aluno.senac.rio>
2026-09-11 21:28:29 -0300

    Versionamento

---
+++ MODULO2/04092026.py
@@ -0,0 +1,26 @@
+## AULA 01 ##
+#print ("sextou! (com feriado)")
+
+import pandas as pd 
+import numpy as np
+
+numeros_impares = [43,55,1,3,11,27,109]
+numeros_seq = [2,3,4,5,6,7]
+print (type(numeros_impares))
+
+serie_impares = pd.Series(numeros_impares)
+print(serie_impares)
+print(type(serie_impares))
+
+print(serie_impares.sum())
+print(serie_impares.mean())
+print(serie_impares.min())
+print(serie_impares.max())
+print(len(serie_impares))
+print(serie_impares.describe())
+print(serie_impares[serie_impares])
+
+serie2_impares = pd.Series(
+    numeros_impares,
+    index =['a','b','c','d','e','f','g'])
+print(serie2_impares)
